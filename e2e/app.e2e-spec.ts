fa1e100b6e404dc557197f4b337d8556303e8f48
Ramesh BN <raga2560@yahoo.com>
2018-01-07 19:19:59 +0530

    e2e working

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,22 +11,25 @@ describe('Example E2E Test', () => {
   // expect(element(by.css('[aria-selected=true] .tab-button-text')) // Grab the title of the selected tab  
        expect(element(by.id('hometitle')) // Grab the title of the selected tab 
         .getAttribute('innerHTML')) // Get the text content
-        .toContain('Ionic Blank'); // Check if it contains the text "Home"
+        .toContain('Insurance Home'); // Check if it contains the text "Home"
+		
 
   });
 
   it('the user can browse to the contact tab and view the ionic twitter handle', () => {
 
     // Click the 'About' tab
-    let btn = element(by.buttonText('Create Insurance Entry'));
+    let btn = element(by.buttonText('Insurances'));
 
+	
+	
 	btn.click();
       // Wait for the page transition
       browser.driver.sleep(5000);
 
-      expect(element(by.id('tradepanel1')) // Grab the label of the list item
+      expect(element(by.buttonText('Transit Insurance')) // Grab the label of the list item
         .getAttribute('innerHTML')) // Get the text content
-        .toContain('Insurance Trade Panel'); // Check if it contains the text "@ionicframework"
+        .toContain('Transit Insurance'); // Check if it contains the text "@ionicframework"
 
     
 
